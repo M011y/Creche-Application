19c39b3ca62f390601968e997f4bca184fecabae
Molly Candon <S00191042@mail.itsligo.ie>
2019-05-05 00:28:02 +0100

    Starting edit applicant

---
+++ Assignment1/Pages/Edit.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Assignment.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Assignment.Pages
+{
+    public class EditModel : PageModel
+    {
+        //brings in database
+        private readonly CrecheContext _db;
+
+        public EditModel(CrecheContext db)
+        {
+            _db = db;
+        }
+
+        //brings in applicant
+        [BindProperty]
+        public Applicant applicant { get; set; }
+
+        //checks ID from button clicked and helps route to correspoding view applicant page
+        public IActionResult OnGet(int id)
+        {
+            applicant = _db.Applicants.Find(id);
+
+            if (applicant == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
