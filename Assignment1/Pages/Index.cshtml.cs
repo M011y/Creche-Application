65ae53da6306dd7c7e2952e03582f903cbc61193
Molly Candon <S00191042@mail.itsligo.ie>
2019-05-06 14:23:38 +0100

    Cookie started

--- Assignment1/Pages/Index.cshtml.cs
+++ Assignment1/Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +10,24 @@ namespace Assignment.Pages
 {
     public class IndexModel : PageModel
     {
+        public string Message { get; set; }
+
         public void OnGet()
         {
+            CookieOptions options = new CookieOptions();
 
+            options.Expires = DateTime.Now.AddDays(365);
+
+            Response.Cookies.Append("CrecheCookie", "Visit", options);
+
+            if (Request.Cookies["CrecheCookie"] != null)
+            {
+                Message = "Welcome back";
+            }
+            else
+            {
+                Message = "First visit";
+            }
         }
     }
 }
