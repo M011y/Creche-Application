876067db2d7fbd29b8821d4fb73559ba56d5d206
Molly Candon <S00191042@mail.itsligo.ie>
2019-05-06 21:20:12 +0100

    Web Server/API started

---
+++ Assignment1/Controllers/CrecheController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Assignment.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CrecheController : ControllerBase
+    {
+        private readonly CrecheContext _context;
+
+        public CrecheController(CrecheContext context)
+        {
+            _context = context;
+
+            if (_context.Applicants.Count() == 0)
+            {
+                _context.Applicants.Add(new Models.Applicant { CFirstName = "Item1" });
+                _context.SaveChanges();
+            }
+        }
+
+        // GET: api/Todo
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Applicant>>> GetApplicants()
+        {
+            return await _context.Applicants.ToListAsync();
+        }
+
+        // GET: api/Todo/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Applicant>> GetApplicant(int ID)
+        {
+            var applicant = await _context.Applicants.FindAsync(ID);
+
+            if (applicant == null)
+            {
+                return NotFound();
+            }
+
+            return applicant;
+        }
+
+        // POST: api/Todo
+        [HttpPost]
+        public async Task<ActionResult<Applicant>> PostApplicant(Applicant item)
+        {
+            _context.Applicants.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetApplicant), new { id = item.ID }, item);
+        }
+
+        // PUT: api/Todo/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutApplicant(int id, Applicant item)
+        {
+            if (id != item.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+        // DELETE: api/Todo/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteApplicant(long ID)
+        {
+            var applicant = await _context.Applicants.FindAsync(ID);
+
+            if (applicant == null)
+            {
+                return NotFound();
+            }
+
+            _context.Applicants.Remove(applicant);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
